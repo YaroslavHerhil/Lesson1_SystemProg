c46f8c98527d84d4df7413e7fd3e5c8d4f9a2c88
Gerg_hn60 <Gerg_hn60@student.itstep.org>
2023-10-04 20:16:47 +0200

    Add project files.

---
+++ Lesson1_SystemProg/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace Lesson1_SystemProg
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnStart = new System.Windows.Forms.Button();
+            btnStop = new System.Windows.Forms.Button();
+            process1 = new System.Diagnostics.Process();
+            comboBox1 = new System.Windows.Forms.ComboBox();
+            textBox1 = new System.Windows.Forms.TextBox();
+            listView1 = new System.Windows.Forms.ListView();
+            button1 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // btnStart
+            // 
+            btnStart.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            btnStart.Location = new System.Drawing.Point(495, 391);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new System.Drawing.Size(112, 33);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += Start_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
+            btnStop.Location = new System.Drawing.Point(26, 390);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new System.Drawing.Size(112, 33);
+            btnStop.TabIndex = 1;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // process1
+            // 
+            process1.StartInfo.Domain = "";
+            process1.StartInfo.LoadUserProfile = false;
+            process1.StartInfo.Password = null;
+            process1.StartInfo.StandardErrorEncoding = null;
+            process1.StartInfo.StandardInputEncoding = null;
+            process1.StartInfo.StandardOutputEncoding = null;
+            process1.StartInfo.UserName = "";
+            process1.SynchronizingObject = this;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "notepad", "calc", "mspaint", "PaintDotNet", "windirstat" });
+            comboBox1.Location = new System.Drawing.Point(260, 391);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new System.Drawing.Size(229, 33);
+            comboBox1.TabIndex = 3;
+            comboBox1.Text = "Select an application";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            textBox1.Location = new System.Drawing.Point(26, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(581, 31);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // listView1
+            // 
+            listView1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            listView1.HideSelection = false;
+            listView1.Location = new System.Drawing.Point(26, 62);
+            listView1.Name = "listView1";
+            listView1.Size = new System.Drawing.Size(581, 306);
+            listView1.TabIndex = 5;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // button1
+            // 
+            button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
+            button1.Location = new System.Drawing.Point(144, 391);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(110, 33);
+            button1.TabIndex = 6;
+            button1.Text = "Refresh";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(640, 436);
+            Controls.Add(button1);
+            Controls.Add(listView1);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lesson1_SystemProg/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lesson1_SystemProg
+{
+    public partial class Form1 : Form
+    {
+        private Process[] allProcesses;
+        string filter = "";
+        public Form1()
+        {
+            InitializeComponent();
+            allProcesses = Process.GetProcesses();
+            process1.StartInfo = new System.Diagnostics.ProcessStartInfo("notepad");
+            ListOfProcessesReset();
+            listView1.Columns.Add("Name", 260);
+            listView1.Columns.Add("Id", 86);
+            listView1.Columns.Add("Start Time", 200);
+            listView1.Columns.Add("Count", 100);
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                process1.Close();
+                process1.StartInfo = new System.Diagnostics.ProcessStartInfo(comboBox1.SelectedItem.ToString());
+            }
+            process1.Start();
+            ListOfProcessesReset();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                string processName = listView1.FocusedItem.ToString();
+                foreach (var process in Process.GetProcessesByName(processName))
+                {
+                    process.Kill();
+
+                }
+                MessageBox.Show($"process \"{processName}\" killed");
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show($"cant kill process \"{x.Message}\"");
+            }
+            ListOfProcessesReset();
+        }
+
+        private void ListOfProcessesReset()
+        {
+            listView1.Items.Clear();
+            
+
+
+
+            foreach (var process in allProcesses)
+            {
+                ListViewItem item = new ListViewItem();
+
+                int pFrom = process.ToString().IndexOf('(') + 1;
+                int pTo = process.ToString().LastIndexOf(')') - pFrom;
+                string processName = process.ToString().Substring(pFrom, pTo);
+                if (processName.Contains(filter))
+                {
+                    string pCount = "0";
+                    string pId = process.Id.ToString();
+                    string pStartTime;
+                    try
+                    {
+                        pStartTime = process.StartTime.ToString();
+
+                    }
+                    catch
+                    {
+                        pStartTime = "Access denied exception";
+                    }
+
+
+                    if (listView1.Items.Count == 0)
+                    {
+                        item.Name = processName;
+                        item.Text = processName;
+                        item.SubItems.Add(pId);
+                        item.SubItems.Add(pStartTime);
+                        item.SubItems.Add(pCount);
+                        listView1.Items.Add(item);
+                    }
+                    bool found = false;
+                    foreach (ListViewItem lvi in listView1.Items)
+                    {
+
+                        if (lvi.SubItems[0].Text == processName)
+                        {
+                            found = true;
+                            lvi.SubItems[3].Text = (int.Parse(lvi.SubItems[3].Text) + 1).ToString();
+                        }
+
+                    }
+                    if (!found)
+                    {
+                        item.Name = processName;
+                        item.Text = processName;
+                        item.SubItems.Add(pId);
+                        item.SubItems.Add(pStartTime);
+                        item.SubItems.Add(pCount);
+                        listView1.Items.Add(item);
+                    }
+
+                }
+
+            }
+        }
+
+        private void btnProcessSelect_Click(object sender, EventArgs e)
+        {
+
+            process1.Close();
+            System.Windows.Forms.Button tempButton = (System.Windows.Forms.Button)sender;
+            process1.StartInfo = new System.Diagnostics.ProcessStartInfo(tempButton.Name.Substring(3));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            filter = textBox1.Text;
+            ListOfProcessesReset();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListOfProcessesReset();
+        }
+    }
+}
